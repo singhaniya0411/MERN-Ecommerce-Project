69bf2c9dc3d6dfa2ad8f18f97762b8291e9862ed
Vishal Singhaniya <vi.singhaniya2956@gmail.com>
2025-02-17 22:22:35 +0530

    Updates Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import Cart from "./pages/Cart";
@@ -19,10 +19,10 @@ import "react-toastify/dist/ReactToastify.css";
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <ToastContainer />
-      <Navbar />
-      <SearchBar />
-      <Routes basename="/MERN-Ecommerce-Project">
+      <Routes>
+        <ToastContainer />
+        <Navbar />
+        <SearchBar />
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
         <Route path="/cart" element={<Cart />} />
