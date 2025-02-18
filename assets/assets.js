232ecbcab2612dcc39b3f365c179b18d0fdcd071
Vishal Singhaniya <vi.singhaniya2956@gmail.com>
2025-02-18 23:39:50 +0530

    Updates

---
+++ assets/assets.js
@@ -0,0 +1,9 @@
+import add_icon from './add_icon.png';
+import image_upload from './image_upload.png';
+import order_icon from './order_icon.png';
+import parcel_icon from './parcel_icon.png';
+import logo from './logo.png'
+
+export const assets = {
+  add_icon, image_upload, order_icon, parcel_icon, logo
+}
