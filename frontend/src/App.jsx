325c15a12e95308b409b8b009b791bd2f9ba50da
Vishal Singhaniya <vi.singhaniya2956@gmail.com>
2025-02-18 22:55:37 +0530

    "Update App.js"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import Cart from "./pages/Cart";
@@ -19,32 +19,31 @@ import "react-toastify/dist/ReactToastify.css";
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <Router>
-        <ToastContainer />
-        <Navbar />
-        <SearchBar />
-        <Routes>
-          <Route path="/MERN-Ecommerce-Project" element={<Home />} />
-          <Route
-            path="/MERN-Ecommerce-Project/collection"
-            element={<Collection />}
-          />
-          <Route path="/MERN-Ecommerce-Project/cart" element={<Cart />} />
-          <Route path="/MERN-Ecommerce-Project/contact" element={<Contact />} />
-          <Route path="/MERN-Ecommerce-Project/login" element={<Login />} />
-          <Route path="/MERN-Ecommerce-Project/orders" element={<Orders />} />
-          <Route
-            path="/MERN-Ecommerce-Project/placeorder"
-            element={<PlaceOrder />}
-          />
-          <Route
-            path="/MERN-Ecommerce-Project/product/:productID"
-            element={<Product />}
-          />
-          <Route path="/MERN-Ecommerce-Project/about" element={<About />} />
-          <Route path="/MERN-Ecommerce-Project/verify" element={<Verify />} />
-        </Routes>
-      </Router>
+      <ToastContainer />
+      <Navbar />
+      <SearchBar />
+      <Routes>
+        <Route path="/MERN-Ecommerce-Project" element={<Home />} />
+        <Route
+          path="/MERN-Ecommerce-Project/collection"
+          element={<Collection />}
+        />
+        <Route path="/MERN-Ecommerce-Project/cart" element={<Cart />} />
+        <Route path="/MERN-Ecommerce-Project/contact" element={<Contact />} />
+        <Route path="/MERN-Ecommerce-Project/login" element={<Login />} />
+        <Route path="/MERN-Ecommerce-Project/orders" element={<Orders />} />
+        <Route
+          path="/MERN-Ecommerce-Project/placeorder"
+          element={<PlaceOrder />}
+        />
+        <Route
+          path="/MERN-Ecommerce-Project/product/:productID"
+          element={<Product />}
+        />
+        <Route path="/MERN-Ecommerce-Project/about" element={<About />} />
+        <Route path="/MERN-Ecommerce-Project/verify" element={<Verify />} />
+      </Routes>
+
       <Footer />
     </div>
   );
